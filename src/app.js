d5028b21d51127a8ebab3b378a7b6e71d78514ba
Andrew Huang <andrew@SYDL009179.vci.local>
2020-01-09 00:22:02 -0800

    Adding helper functions + finished up lambda

--- src/app.js
+++ src/app.js
@@ -1,17 +1,30 @@
 import axios from 'axios';
-import urlBuilder from '../helpers/utils'
+import {urlBuilder, messageBuilder} from '../helpers/utils'
+import AWS from 'aws-sdk';
+
 require('dotenv').config();
 
-exports.handler = (event, context, callback) => {
+exports.handler = (event, context) => {
     const lat = '49.2859524'
     const long = '-123.1137529'
     let url = urlBuilder(lat, long);
 
     axios.get(url)
         .then(res => {
-
+            const sns = AWS.SNS();
+            const params = {
+                Message: messageBuilder(res),
+                PhoneNumber: process.env.PHONE_NUMBER,
+                MessageAttributes: {
+                    'AWS.SNS.SMS.SenderID': {
+                        'DataType': 'String',
+                        'StringValue': 'Weather Notification Service'
+                    }
+                }
+            }
+            sns.publish(params, context.done);
         })
         .catch(err => {
-            callback(err);
+            context.fail(err);
         })
 };
